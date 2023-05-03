110d754586582880c260466939096cb48d29be58
kauevette <kauezinho.vette@hotmail.com>
2023-05-03 16:50:53 -0300

    Criado o meu perfil online

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Meu Perfil</title>
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <img src="assets/perfil.jpg" alt="perfil" class="profile">
+            <div class="container-text">
+                <h1 class="name">Kauê Jardim de Vette</h1>
+                <h3 class="desc">Desenvolvedor Front-End <br>
+                <img src="assets/localizacao.png" alt="ender" class="address-icon">Mirassol, São Paulo, Brasil</h3>
+            </div>
+            <div class="socials">
+                <a href="#" target="_blank"><img src="assets/github.png" alt="github"></a>
+                <a href="#" target="_blank"><img src="assets/linkedin.png" alt="linkedin"></a>
+                <a href="#" target="_blank"><img src="assets/instagram.png" alt="instagram"></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
